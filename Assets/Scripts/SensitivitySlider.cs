13b886724fe2f874033fd746b425762578b0fbde
AAYFlower <44514581+AAYFlower@users.noreply.github.com>
2024-02-23 23:36:05 -0800

    adsfasdf

--- Assets/Scripts/SensitivitySlider.cs
+++ Assets/Scripts/SensitivitySlider.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class SensitivitySlider : MonoBehaviour
 {
     // Start is called before the first frame update
     public TextMeshProUGUI SliderText;
+    public TMP_InputField inputField;
+    public Slider slider;
     void Start()
     {
         SliderText.text = Settings.sensitivitySettings.ToString("0.00");
@@ -14,6 +18,12 @@ public class SensitivitySlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void inputfieldvalueRecieved()
+    {
+
 
     }
 
